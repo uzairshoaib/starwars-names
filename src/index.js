ae4e1755c8dec73aa6109cb2649af9398b635900
Uzair Shoaib <uzairshoaib.dev@gmail.com>
2019-11-11 01:20:49 +0500

    feat(random): add ability to get an array of starwars names
    if you pass a number to the random function, you will receive an array with that number of random
    items
    
    #2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@
 
 var starWarsNames = require('./starwars-names.json');
 var uniqueRandomArray = require('unique-random-array');
+var getRandomItem = uniqueRandomArray(starWarsNames);
+
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames),
+  random: random,
+}
+function random(number) {
+  if(typeof number === 'undefined') {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for(var i=0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
 }
